a5882d04d599f64d122d91ff5e61e7cc9985b205
MohamedAdel5 <mohamed.adel_5@yahoo.com>
2019-03-22 10:29:55 +0200

    Added setters,getter,default constructor and operator overloading functions

--- Project_Code_S2019/Restaurant/Rest/Order.cpp
+++ Project_Code_S2019/Restaurant/Rest/Order.cpp
@@ -4,16 +4,34 @@ Order::Order(int id, ORD_TYPE r_Type, REGION r_region)
 {
 	ID = (id>0&&id<1000)?id:0;	//1<ID<999
 	type = r_Type;
-	Region = r_region;	
+	Region = r_region;
 }
-
+Order::Order() {
+	//To avoid errors if a node is instantiated without assigning a value to its item of type Order
+}
 Order::~Order()
 {
+}
+
+void Order::SetID(const int& id)
+{
+	ID = (id > 0 && id < 1000) ? id : 0;
 }
 
 int Order::GetID()
 {
 	return ID;
+}
+
+void Order::SetRegion(const REGION & r_region)
+{
+	Region = r_region;
+}
+
+
+void Order::SetType(const ORD_TYPE & r_Type)
+{
+	type = r_Type;
 }
 
 
@@ -35,4 +53,40 @@ void Order::SetDistance(int d)
 int Order::GetDistance() const
 {
 	return Distance;
+}
+
+void Order::setPriority(const double pr)
+{
+	Priority =(pr > 0)?pr:0;
+}
+
+double Order::getPriority() const
+{
+	return Priority;
+}
+
+bool Order::operator >(const Order& o) const
+{
+	if (Priority > o.Priority)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Order::operator==(const int id) const
+{
+	if (ID == id)
+	{
+		return true;
+	}
+	return false;
+}
+
+
+	/*---------------------------------------------for testing only------------------------------------------*/
+
+ostream& operator <<(ostream& o, Order& ord) {
+	o << "order of id:" << ord.GetID() << " and type: " << ord.GetType() << " and region: " << ord.GetRegion() << endl;
+	return o;
 }
--- Project_Code_S2019/Restaurant/Rest/Order.h
+++ Project_Code_S2019/Restaurant/Rest/Order.h
@@ -3,36 +3,48 @@
 
 #include "..\Defs.h"
 
+#include <iostream>		//for testing the output
+using namespace std;	//for testin the output
+
 class Order
 {
 
 protected:
-	int ID;         //Each order has a unique ID (from 1 --> 999 )
-	ORD_TYPE type;		//order type: Normal, Frozen, VIP
-	REGION Region;  //Region of this order
-	int Distance;	//The distance (in meters) between the order location and the resturant 
-	
-	double totalMoney;	//Total order money
-
+	int ID;								//Each order has a unique ID (from 1 --> 999 )
+	ORD_TYPE type;						//order type: Normal, Frozen, VIP
+	REGION Region;						//Region of this order
+	int Distance;						//The distance (in meters) between the order location and the resturant 
+	double totalMoney;					//Total order money
 	int ArrTime, ServTime, FinishTime;	//arrival, service start, and finish times
-	
-	double priority;
+	double Priority;					//Priority of the order (ony for VIP orders)
 	//
 	// TODO: Add More Data Members As Needed
 	//
 
 public:
 	Order(int ID, ORD_TYPE r_Type, REGION r_region);
+	Order();
 	virtual ~Order();
 
+	void SetID(const int& id);
 	int GetID();
 
+	
+
+	void SetType(const ORD_TYPE& r_Type);
 	int GetType() const;
+
+	void SetRegion(const REGION& r_region);
 	REGION GetRegion() const;
 
 	void SetDistance(int d);
 	int GetDistance() const;
 
+	void setPriority(const double pr); //only for VIP orders
+	double getPriority() const;	 //only for VIP orders
+
+	bool operator >(const Order& o) const;	// compares the priority of two orders
+	bool operator ==(const int id) const;	// compares the ID of an order with a given ID
 	//
 	// TODO: Add More Member Functions As Needed
 	//

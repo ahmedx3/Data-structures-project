fe4364277b12a5b24fdbf0797ab9b663be6cb2f6
ahmedx3 <ahmed.moustafaa@outlook.com>
2019-03-22 23:05:11 +0200

    Added functions to motorcycle

--- Project_Code_S2019/Restaurant/Rest/Motorcycle.cpp
+++ Project_Code_S2019/Restaurant/Rest/Motorcycle.cpp
@@ -5,6 +5,20 @@ Motorcycle::Motorcycle()
 {
 }
 
+Motorcycle::Motorcycle(int i, ORD_TYPE t, int s, REGION r, STATUS ss)
+{
+	ID = i;
+	type = t;
+	speed = s;
+	region = r;
+	status = ss;
+}
+
+bool Motorcycle::operator>(Motorcycle & m2)
+{
+	return speed > m2.speed;
+}
+
 
 Motorcycle::~Motorcycle()
 {
